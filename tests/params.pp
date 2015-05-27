e8bfb4d9494c273b04265e93282a4c71ef7b382d
Sebastien Varrette <sebastien.varrette@uni.lu>
2015-05-27 11:35:21 +0530

    add test directory
    Signed-off-by: Sebastien Varrette <sebastien.varrette@uni.lu>

---
+++ tests/params.pp
@@ -0,0 +1,28 @@
+# File::      <tt>params.pp</tt>
+# Author::    UL HPC Team aka. S. Varrette, H. Cartiaux, V. Plugaru, S. Diehl (hpc-sysadmins@uni.lu)
+# Copyright:: Copyright (c) 2015 UL HPC Team aka. S. Varrette, H. Cartiaux, V. Plugaru, S. Diehl
+# License::   Gpl-3.0
+#
+# ------------------------------------------------------------------------------
+# You need the 'future' parser to be able to execute this manifest (that's
+# required for the each loop below).
+#
+# Thus execute this manifest in your vagrant box as follows:
+#
+#      sudo puppet apply -t --parser future /vagrant/tests/params.pp
+#
+#
+
+include 'sysadmins::params'
+
+$names = ["ensure", "protocol", "port", "packagename"]
+
+notice("sysadmins::params::ensure = ${sysadmins::params::ensure}")
+notice("sysadmins::params::protocol = ${sysadmins::params::protocol}")
+notice("sysadmins::params::port = ${sysadmins::params::port}")
+notice("sysadmins::params::packagename = ${sysadmins::params::packagename}")
+
+#each($names) |$v| {
+#    $var = "sysadmins::params::${v}"
+#    notice("${var} = ", inline_template('<%= scope.lookupvar(@var) %>'))
+#}
